71d77a3776946a46acad03a8484f8fa3c4b42bf9
Vijay A <avijayr@protonmail.com>
2021-07-18 09:05:08 +0530

    Removed references to Franz paid plans features:   - serviceLimit   - planSelection   - trialStatusBar
    and other Franz features that were for different tiers of subscription.

--- app/Controllers/Http/StaticController.js
+++ app/Controllers/Http/StaticController.js
@@ -9,63 +9,18 @@ class StaticController {
     response,
   }) {
     return response.send({
-      needToWaitToProceed: false,
       isSpellcheckerPremiumFeature: false,
-      isSpellcheckerIncludedInCurrentPlan: true,
       isServiceProxyEnabled: true,
-      isServiceProxyIncludedInCurrentPlan: true,
       isServiceProxyPremiumFeature: true,
       isWorkspacePremiumFeature: false,
       isWorkspaceEnabled: true,
       isAnnouncementsEnabled: true,
       isSettingsWSEnabled: false,
-      isServiceLimitEnabled: false,
-      serviceLimitCount: 0,
       isCommunityRecipesPremiumFeature: false,
-      isCommunityRecipesIncludedInCurrentPlan: true,
-      isCustomUrlIncludedInCurrentPlan: true,
       isMagicBarEnabled: true,
-      isTeamManagementIncludedInCurrentPlan: true,
       isTodosEnabled: true,
-      isTodosIncludedInCurrentPlan: true,
-      defaultTrialPlan: "franz-pro-yearly",
       subscribeURL: "https://getferdi.com",
-      planSelectionURL: "https://getferdi.com",
-      isMagicBarEnabled: true,
       hasInlineCheckout: true,
-      isPlanSelectionEnabled: false,
-      isTrialStatusBarEnabled: false,
-      canSkipTrial: true,
-      pricingConfig: {
-        currency: "$",
-        currencyID: "USD",
-        plans: {
-          personal: {
-            monthly: {
-              id: "ferdi-free",
-              price: 0,
-              billed: 0
-            },
-            yearly: {
-              id: "ferdi-completely-free",
-              price: 0,
-              billed: 0
-            }
-          },
-          pro: {
-            monthly: {
-              id: "ferdi-still-free",
-              price: 0,
-              billed: 0
-            },
-            yearly: {
-              id: "ferdi-forever-free",
-              price: 0,
-              billed: 0
-            }
-          }
-        }
-      }
     });
   }
 
@@ -76,27 +31,12 @@ class StaticController {
     return response.send([]);
   }
 
-  // Payment plans availible
-  plans({
-    response,
-  }) {
-    return response.send({
-      month: {
-        id: 'franz-supporter-license',
-        price: 99,
-      },
-      year: {
-        id: 'franz-supporter-license-year-2019',
-        price: 99,
-      },
-    });
-  }
-
   // Return list of popular recipes (copy of the response Franz's API is returning)
   popularRecipes({
     response,
   }) {
     return response.send([{
+      // TODO: Why is this list hardcoded?
       author: 'Stefan Malzner <stefan@adlk.io>',
       featured: false,
       id: 'slack',
